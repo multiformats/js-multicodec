137a2a509a3291890098134be432ee392bcc5f3a
Friedel Ziegelmayer <dignifiedquire@gmail.com>
2016-12-14 15:01:46 +0100

    docs(api): first pass

--- src/index.js
+++ src/index.js
@@ -1,4 +1,16 @@
+/**
+ * Implementation of the multicodec specification.
+ *
+ * @module multicodec
+ * @example
+ * const multicodec = require('multicodec')
+ *
+ * const prefixedProtobuf = multicodec.addPrefix('protobuf', protobufBuffer)
+ * // prefixedProtobuf 0x50...
+ *
+ */
 'use strict'
+
 const varint = require('varint')
 const codecNameToCodeVarint = require('./varint-table')
 const codeToCodecName = require('./name-table')
@@ -6,6 +18,13 @@ const util = require('./util')
 
 exports = module.exports
 
+/**
+ * Prefix a buffer with a multicodec-packed.
+ *
+ * @param {string|number} multicodecStrOrCode
+ * @param {Buffer} data
+ * @returns {Buffer}
+ */
 exports.addPrefix = (multicodecStrOrCode, data) => {
   let prefix
 
@@ -21,14 +40,24 @@ exports.addPrefix = (multicodecStrOrCode, data) => {
   return Buffer.concat([prefix, data])
 }
 
+/**
+ * Decapsulate the multicodec-packed prefix from the data.
+ *
+ * @param {Buffer} data
+ * @returns {Buffer}
+ */
 exports.rmPrefix = (data) => {
   varint.decode(data)
   return data.slice(varint.decode.bytes)
 }
 
+/**
+ * Get the codec of the prefixed data.
+ * @param {Buffer} prefixedData
+ * @returns {string}
+ */
 exports.getCodec = (prefixedData) => {
   const code = util.varintBufferDecode(prefixedData)
   const codecName = codeToCodecName[code.toString('hex')]
   return codecName
 }
-
