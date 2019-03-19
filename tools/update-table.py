2e5367f340bcd31bcce1d36fdb132c2d5fcb9ad1
Volker Mische <volker.mische@gmail.com>
2019-03-19 15:58:12 +0100

    fix: update the multicodec table
    The upstream table changed its formatting, hence also the script needed
    some changes. The ordering of the codecs within a group is now according
    to the codec code value.
    
    Closes #40.

--- tools/update-table.py
+++ tools/update-table.py
@@ -3,6 +3,7 @@
 import csv
 import os
 import sys
+from collections import OrderedDict
 
 # This is relative to where this script resides. Though you can also define
 # an absolute path
@@ -16,6 +17,7 @@ HEADER = '''\
 'use strict'
 '''
 
+
 def padded_hex(hexstring):
     """Creates a padded (starting with a 0 if odd) hex string"""
     number = int(row['code'], 16)
@@ -25,6 +27,7 @@ def padded_hex(hexstring):
     else:
         prefix = '0x'
     return prefix + hexbytes
+
 
 def unique_code(codecs):
     """Returns a list where every code exists only one.
@@ -42,24 +45,34 @@ def unique_code(codecs):
         unique.append(codec)
     return unique
 
-parsed = []
+
+# Preserve the order from earlier versions. New tags are appended
+parsed = OrderedDict([
+    ('serialization', []),
+    ('multiformat', []),
+    ('multihash', []),
+    ('multiaddr', []),
+    ('ipld', []),
+    ('namespace', []),
+    ('key', []),
+    ('holochain', []),
+])
+
 multicodec_reader = csv.DictReader(sys.stdin, skipinitialspace=True)
 for row in multicodec_reader:
-    # Skip over headlines and not yet defined codes
-    if row['code'] and row['code'] != '0x':
-        code = padded_hex(row['code'])
-        name_const = row['codec'].upper().replace('-', '_')
-        name_human = row['codec']
-        parsed.append({
-            'const': name_const,
-            'human': name_human,
-            'code': code,
-        })
-    # add headlines
-    if row['codec'] and not row['description'] and not row['code']:
-        parsed.append({
-            'headline': row['codec']
-        })
+    code = padded_hex(row['code'])
+    name_const = row['name'].upper().replace('-', '_')
+    name_human = row['name']
+    tag = row['tag']
+    value = {
+        'const': name_const,
+        'human': name_human,
+        'code': code
+    }
+    if tag not in parsed:
+        parsed[tag] = []
+
+    parsed[tag].append(value)
 
 tools_dir = os.path.dirname(os.path.abspath(__file__))
 output_dir = os.path.join(tools_dir, DEFAULT_OUTPUT_DIR)
@@ -67,11 +80,9 @@ output_dir = os.path.join(tools_dir, DEFAULT_OUTPUT_DIR)
 print_file = os.path.join(output_dir, 'base-table.js')
 with open(print_file, 'w') as ff:
     ff.write(HEADER)
-    for index, codec in enumerate(parsed):
-        if 'headline' in codec:
-            ff.write("\n// {headline}\n".format(**codec))
-        else:
-            # print('{} {} {:02x}'.format(codec['human'], codec['code'], codec['code']))
+    for tag, codecs in parsed.items():
+        ff.write(f"\n// {tag}\n")
+        for codec in codecs:
             hexstring = codec['code'][2:]
             ff.write("exports['{}'] = Buffer.from('{}', 'hex')\n"
                      .format(codec['human'], hexstring))
@@ -80,25 +91,25 @@ constants_file = os.path.join(output_dir, 'constants.js')
 with open(constants_file, 'w') as ff:
     ff.write(HEADER)
     ff.write('module.exports = Object.freeze({\n')
-    for index, codec in enumerate(parsed):
-        if index:
-            ff.write(',\n')
-        if 'headline' in codec:
-            ff.write("\n  // {headline}".format(**codec))
-        else:
+    for tagindex, (tag, codecs) in enumerate(parsed.items()):
+        ff.write(f"\n  // {tag}\n")
+        for codecindex, codec in enumerate(codecs):
             ff.write("  {const}: {code}".format(**codec))
+            # Add a trailing comma except for the last item in the dict
+            if tagindex < len(parsed) - 1 or codecindex < len(codecs) - 1:
+                ff.write(',\n')
     ff.write('\n})\n')
 
 print_file = os.path.join(output_dir, 'print.js')
 with open(print_file, 'w') as ff:
     ff.write(HEADER)
     ff.write('module.exports = Object.freeze({\n')
-    unique = unique_code(parsed)
-    for index, codec in enumerate(unique):
-        if index:
-            ff.write(',\n')
-        if 'headline' in codec:
-            ff.write("\n  // {headline}".format(**codec))
-        else:
+    for tagindex, (tag, codecs) in enumerate(parsed.items()):
+        ff.write(f"\n  // {tag}\n")
+        unique = unique_code(codecs)
+        for codecindex, codec in enumerate(unique):
             ff.write("  {code}: '{human}'".format(**codec))
+            # Add a trailing comma except for the last item in the dict
+            if tagindex < len(parsed) - 1 or codecindex < len(codecs) - 1:
+                ff.write(',\n')
     ff.write('\n})\n')
