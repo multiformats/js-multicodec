4dbd20c3f4cf8e3f2d32cb185a93622baf29eef1
Mikeal Rogers <mikeal.rogers@gmail.com>
2019-09-18 19:31:25 +0000

    feat: reduced bundle size

---
+++ tools/update-table.js
@@ -0,0 +1,49 @@
+const bent = require('bent')
+const HEADER = `
+// THIS FILE IS GENERATED, DO NO EDIT MANUALLY
+// For more information see the README.md
+'use strict'
+
+const { numberToBuffer } = require('../src/util')
+
+const table = {
+`
+const FOOTER = `
+}
+for (let [k,v] of Object.entries(table)) {
+  table[k] = numberToBuffer(v)
+}
+module.exports = Object.freeze(table)
+`
+
+const get = bent('string')
+const url = 'https://raw.githubusercontent.com/multiformats/multicodec/master/table.csv'
+const fs = require('fs')
+
+const parse = async function * () {
+  let str = await get(url)
+  let lines = str.split('\n')
+  let header = lines.shift()
+  for (let line of lines) {
+    if (!line.length) continue
+    let [name, tag, code, desc] = line.split(',')
+    yield { name: name.trim(), tag: tag.trim(), code: parseInt(code.trim(), 16) }
+  }
+}
+
+const run = async () => {
+  let str = HEADER
+  let first = true
+  for await (let entry of parse()) {
+    key = entry.name
+    if (key.includes('-')) key = `"${key}"`
+    console.log(entry)
+    if (!first) str += ',\n  '
+    else str += ' ' 
+    str += `${key}: ${entry.code}`
+    first = false
+  }
+  str += FOOTER
+  fs.writeFileSync(__dirname + '/../src/base-table.js', str)
+}
+run()
