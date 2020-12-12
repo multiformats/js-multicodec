b7643416ad9b690d7eaa07804bc9ac6c81eae04f
Henrique Dias <hacdias@gmail.com>
2020-12-12 10:27:01 +0100

    refactor: rename functions to explicit names
    Breaking refactorings:
    - Renamed getCodec to getNameFromData
    - Renamed getName to getNameFromCode
    - Renamed getNumber to getCodeFromName
    - Renamed getCode to getCodeFromData
    - Renamed getCodeVarint to getVarintFromName
    - Renamed getVarint to getVarintFromCode
    - getVarintFromCode returns Uint8Array instead of Array
    
    For backwards compatibility, all old names were kept as alias,
    and getVarint returns a regular Array as it did before. This does
    not break compatibility. They are all marked as @deprecated.
    
    License: MIT
    Signed-off-by: Henrique Dias <hacdias@gmail.com>

--- test/multicodec.spec.js
+++ test/multicodec.spec.js
@@ -13,56 +13,56 @@ describe('multicodec', () => {
   it('add prefix through multicodec (string)', () => {
     const buf = uint8ArrayFromString('hey')
     const prefixedBuf = multicodec.addPrefix('protobuf', buf)
-    expect(multicodec.getCodec(prefixedBuf)).to.equal('protobuf')
+    expect(multicodec.getNameFromData(prefixedBuf)).to.equal('protobuf')
     expect(buf).to.eql(multicodec.rmPrefix(prefixedBuf))
   })
 
   it('add prefix through code (code)', () => {
     const buf = uint8ArrayFromString('hey')
     const prefixedBuf = multicodec.addPrefix(uint8ArrayFromString('70', 'base16'), buf)
-    expect(multicodec.getCodec(prefixedBuf)).to.equal('dag-pb')
+    expect(multicodec.getNameFromData(prefixedBuf)).to.equal('dag-pb')
     expect(buf).to.eql(multicodec.rmPrefix(prefixedBuf))
   })
 
   it('add multibyte varint prefix (eth-block) through multicodec (string)', () => {
     const buf = uint8ArrayFromString('hey')
     const prefixedBuf = multicodec.addPrefix('eth-block', buf)
-    expect(multicodec.getCodec(prefixedBuf)).to.equal('eth-block')
+    expect(multicodec.getNameFromData(prefixedBuf)).to.equal('eth-block')
     expect(buf).to.eql(multicodec.rmPrefix(prefixedBuf))
   })
 
   it('returns code via codec name', () => {
-    const code = multicodec.getCodeVarint('keccak-256')
+    const code = multicodec.getVarintFromName('keccak-256')
     expect(code).to.eql(uint8ArrayFromString('1b', 'base16'))
   })
 
   it('returns code from prefixed data', () => {
     const buf = uint8ArrayFromString('hey')
     const prefixedBuf = multicodec.addPrefix('dag-cbor', buf)
-    const code = multicodec.getCode(prefixedBuf)
+    const code = multicodec.getCodeFromData(prefixedBuf)
     expect(code).to.eql(multicodec.DAG_CBOR)
   })
 
   it('returns varint from code', () => {
-    const code = multicodec.getVarint(multicodec.KECCAK_256)
-    expect(code).to.eql([0x1b])
+    const code = multicodec.getVarintFromCode(multicodec.KECCAK_256)
+    expect(code).to.eql(Uint8Array.from([0x1b]))
   })
 
   it('returns the codec name from code', () => {
-    expect(multicodec.getName(144)).to.eql('eth-block')
-    expect(multicodec.getName(112)).to.eql('dag-pb')
-    expect(multicodec.getName(0xb201)).to.eql('blake2b-8')
+    expect(multicodec.getNameFromCode(144)).to.eql('eth-block')
+    expect(multicodec.getNameFromCode(112)).to.eql('dag-pb')
+    expect(multicodec.getNameFromCode(0xb201)).to.eql('blake2b-8')
   })
 
   it('returns the codec number from name', () => {
-    expect(multicodec.getNumber('eth-block')).to.eql(144)
-    expect(multicodec.getNumber('dag-pb')).to.eql(112)
-    expect(multicodec.getNumber('blake2b-8')).to.eql(0xb201)
+    expect(multicodec.getCodeFromName('eth-block')).to.eql(144)
+    expect(multicodec.getCodeFromName('dag-pb')).to.eql(112)
+    expect(multicodec.getCodeFromName('blake2b-8')).to.eql(0xb201)
   })
 
   it('returns all codec numbers from names', () => {
     for (const name in nameToCode) {
-      expect(multicodec.getNumber(/** @type {CodecName} */(name))).to.eql(nameToCode[name])
+      expect(multicodec.getCodeFromName(/** @type {CodecName} */(name))).to.eql(nameToCode[name])
     }
   })
 
@@ -73,40 +73,49 @@ describe('multicodec', () => {
   })
 
   it('returns the name from codec number', () => {
-    expect(multicodec.getName(144)).to.eql('eth-block')
-    expect(multicodec.getName(112)).to.eql('dag-pb')
-    expect(multicodec.getName(0x0111)).to.eql('udp')
-    expect(multicodec.getName(0xb201)).to.eql('blake2b-8')
+    expect(multicodec.getNameFromCode(144)).to.eql('eth-block')
+    expect(multicodec.getNameFromCode(112)).to.eql('dag-pb')
+    expect(multicodec.getNameFromCode(0x0111)).to.eql('udp')
+    expect(multicodec.getNameFromCode(0xb201)).to.eql('blake2b-8')
 
-    expect(multicodec.getName(multicodec.ETH_BLOCK)).to.eql('eth-block')
-    expect(multicodec.getName(multicodec.DAG_PB)).to.eql('dag-pb')
-    expect(multicodec.getName(multicodec.UDP)).to.eql('udp')
-    expect(multicodec.getName(multicodec.BLAKE2B_8)).to.eql('blake2b-8')
+    expect(multicodec.getNameFromCode(multicodec.ETH_BLOCK)).to.eql('eth-block')
+    expect(multicodec.getNameFromCode(multicodec.DAG_PB)).to.eql('dag-pb')
+    expect(multicodec.getNameFromCode(multicodec.UDP)).to.eql('udp')
+    expect(multicodec.getNameFromCode(multicodec.BLAKE2B_8)).to.eql('blake2b-8')
   })
 
   it('returns p2p when 0x01a5 is used', () => {
     // `ipfs` and `p2p` are assigned to `0x01a5`, `ipfs` is deprecated
-    expect(multicodec.getName(0x01a5)).to.eql('p2p')
+    expect(multicodec.getNameFromCode(0x01a5)).to.eql('p2p')
   })
 
-  it('throws error on unknown codec name when getting the code', () => {
+  it('throws error on unknown codec name when getting the varint from name', () => {
     expect(() => {
       // @ts-expect-error
-      multicodec.getCodeVarint('this-codec-doesnt-exist')
+      multicodec.getVarintFromName('this-codec-doesnt-exist')
     }).to.throw(
-      'Codec `this-codec-doesnt-exist` not found'
+      'Codec "this-codec-doesnt-exist" not found'
     )
   })
 
-  it('throws error on unknown codec name when getting the codec', () => {
+  it('throws error on unknown codec name when getting the code from name', () => {
+    expect(() => {
+      // @ts-expect-error
+      multicodec.getCodeFromName('this-codec-doesnt-exist')
+    }).to.throw(
+      'Codec "this-codec-doesnt-exist" not found'
+    )
+  })
+
+  it('throws error on unknown codec name when getting the name from the code', () => {
     const code = uint8ArrayFromString('ffee', 'base16')
 
     const buf = uint8ArrayFromString('hey')
     const prefixedBuf = multicodec.addPrefix(code, buf)
     expect(() => {
-      multicodec.getCodec(prefixedBuf)
+      multicodec.getNameFromData(prefixedBuf)
     }).to.throw(
-      'Code 65518 not found'
+      'Code "65518" not found'
     )
   })
 })
