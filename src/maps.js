6b29756c86adf42b4323e3ff1082cafee84fe271
Hugo Dias <hugomrdias@gmail.com>
2021-03-01 18:44:50 +0000

    fix: update aegir and revert varint
    This PR upgrade to aegir v31 and reverts varint to 5.0.2 to dedupe upstream.
    
    New base table with 1 new codec "thread"
    
    Notes on the aegir v31 release https://github.com/ipfs/aegir/releases/tag/v31.0.0

--- src/maps.js
+++ src/maps.js
@@ -3,6 +3,8 @@
 /** @typedef {import('./generated-types').ConstantCodeMap} ConstantCodeMap */
 /** @typedef {import('./generated-types').NameUint8ArrayMap} NameUint8ArrayMap */
 /** @typedef {import('./generated-types').CodeNameMap} CodeNameMap */
+/** @typedef {import('./generated-types').CodecName} CodecName */
+/** @typedef {import('./generated-types').CodecConstant} CodecConstant */
 
 const { baseTable } = require('./generated-table')
 const varintEncode = require('./util').varintEncode
@@ -11,15 +13,17 @@ const nameToVarint = /** @type {NameUint8ArrayMap} */ ({})
 const constantToCode = /** @type {ConstantCodeMap} */({})
 const codeToName = /** @type {CodeNameMap} */({})
 
+// eslint-disable-next-line guard-for-in
 for (const name in baseTable) {
-  const code = baseTable[name]
-  nameToVarint[name] = varintEncode(code)
+  const codecName = /** @type {CodecName} */(name)
+  const code = baseTable[codecName]
+  nameToVarint[codecName] = varintEncode(code)
 
-  const constant = name.toUpperCase().replace(/-/g, '_')
+  const constant = /** @type {CodecConstant} */(codecName.toUpperCase().replace(/-/g, '_'))
   constantToCode[constant] = code
 
   if (!codeToName[code]) {
-    codeToName[code] = name
+    codeToName[code] = codecName
   }
 }
 
